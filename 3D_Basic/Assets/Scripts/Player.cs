21bd3176410f882ad78049761e314b65d4cc8dae
go2665 <77224762+go2665@users.noreply.github.com>
2022-03-22 13:54:06 +0900

    함정 완료

--- 3D_Basic/Assets/Scripts/Player.cs
+++ 3D_Basic/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 // 이 스크립트를 가진 게임오브젝트는 무조건 Animator가 있다(없으면 만든다)
 [RequireComponent(typeof(Animator))]
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IDead
 {
     /// 9시 45분까지 구현해보기(Input System 사용)
     /// WASD로 캐릭터 움직이기
@@ -24,6 +24,8 @@ public class Player : MonoBehaviour
 
     private PlayerControls pc = null;   // 입력 처리용 클래스
     private Rigidbody rigid = null;
+
+    private bool isAlive = true;
 
     // 오브젝트가 만들어진 직후에 실행
     private void Awake()
@@ -62,38 +64,41 @@ public class Player : MonoBehaviour
     {
         // 현재 위치 + 캐릭터가 바라보는 방향으로 1초에 moveSpeed씩 이동
         rigid.MovePosition(
-            rigid.position + 
+            rigid.position +
             transform.forward * moveInput * moveSpeed * Time.fixedDeltaTime);
 
         // 현재 각도 * 추가각도
         rigid.MoveRotation(
-            rigid.rotation * 
-            Quaternion.AngleAxis(spinInput * spinSpeed * Time.fixedDeltaTime, Vector3.up) );
+            rigid.rotation *
+            Quaternion.AngleAxis(spinInput * spinSpeed * Time.fixedDeltaTime, Vector3.up));
     }
 
     // WASD를 눌렀을 때 실행될 함수
     public void Move(InputAction.CallbackContext context)
-    {        
-        Vector2 input = context.ReadValue<Vector2>(); // 입력값을 받아서 회전 정도랑 이동 정도를 받아옴
-        spinInput = input.x;    //A(1) D(-1)
-        moveInput = input.y;    //W(1) S(-1)
+    {
+        if (isAlive)
+        {
+            Vector2 input = context.ReadValue<Vector2>(); // 입력값을 받아서 회전 정도랑 이동 정도를 받아옴
+            spinInput = input.x;    //A(1) D(-1)
+            moveInput = input.y;    //W(1) S(-1)
 
-        if (context.started)
-        {
-            anim.SetBool("IsMove", true);   // 누른 직후에 이동 애니메이션 시작
+            if (context.started)
+            {
+                anim.SetBool("IsMove", true);   // 누른 직후에 이동 애니메이션 시작
+            }
+            if (context.canceled)
+            {
+                anim.SetBool("IsMove", false);  // 키를 땠을 때 Idle 애니메이션 시작
+            }
+            //if( moveInput != 0.0f )           // 회전만 할 때는 이동 애니메이션이 안나오게 하고 싶을 때
+            //{
+            //    anim.SetBool("IsMove", true);
+            //}
+            //else
+            //{
+            //    anim.SetBool("IsMove", false);
+            //}
         }
-        if (context.canceled)
-        {
-            anim.SetBool("IsMove", false);  // 키를 땠을 때 Idle 애니메이션 시작
-        }
-        //if( moveInput != 0.0f )           // 회전만 할 때는 이동 애니메이션이 안나오게 하고 싶을 때
-        //{
-        //    anim.SetBool("IsMove", true);
-        //}
-        //else
-        //{
-        //    anim.SetBool("IsMove", false);
-        //}
     }
 
     // 스페이스 키를 눌렀을 때 실행될 함수
@@ -103,4 +108,30 @@ public class Player : MonoBehaviour
         anim.SetTrigger("OnUseItem");   // 스페이스 키를 눌렀을 때 트리거 실행        
     }
 
+    public void OnDead()
+    {
+        if (isAlive)
+        {
+            Debug.Log("플레이어 사망");
+
+            // 사망 연출(죽으면 플레이어는 뒤로 넘어진다. rigidbody 사용)        
+            rigid.constraints = RigidbodyConstraints.None;      // 회전과 이동 묶어두었던 것을 풀기
+            rigid.drag = 0;             // 마찰력 기본값으로 복구
+            rigid.angularDrag = 0.05f;
+            //rigid.AddForce(-transform.forward * 3.0f);    // 플레이어의 뒤쪽방향으로 힘을 가하기
+            rigid.AddForceAtPosition(-transform.forward * 3.0f, transform.position + new Vector3(0, 1.5f, 0));
+
+            CapsuleCollider collider = GetComponent<CapsuleCollider>();
+            collider.radius = 0.2f; // 충돌영역 반지름 줄이기
+            //Destroy(collider, 5.0f);
+
+            // 중복 사망 방지
+            isAlive = false;
+
+            // 죽었을 때 이동 처리 안하기 위한 처리
+            spinInput = 0.0f;
+            moveInput = 0.0f;
+            anim.SetBool("IsMove", false);
+        }
+    }
 }
--- 3D_Basic/Assets/Scripts/Trap.cs
+++ 3D_Basic/Assets/Scripts/Trap.cs
@@ -10,9 +10,36 @@ public class Trap : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    // 내 게임 오브젝트가 가지고 있는 트리거로 설정된 컬라이더에 다른 컬라이더가 들어왔을 때 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{other.name}가 함정을 밟음");
-        anim.SetTrigger("TrapActive");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log($"{other.name}가 함정을 밟음");
+            anim.SetTrigger("TrapActive");
+
+            IDead dead = other.gameObject.GetComponent<IDead>();
+            if(dead !=null)
+            { 
+                dead.OnDead();
+            }            
+        }
     }
+
+    // 내 게임 오브젝트가 가지고 있는 트리거로 설정된 컬라이더에 다른 컬라이더가 나갔을 때
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log($"{other.name}가 함정에서 나감");
+            anim.SetTrigger("TrapDeactivate");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);        
+    }
+
 }
