1dc690c53785cba10e320a0ca03cc395f1172d5e
go2665 <77224762+go2665@users.noreply.github.com>
2022-03-29 15:00:53 +0900

    터렛 기능 추가

--- 3D_Basic/Assets/Scripts/Bullet.cs
+++ 3D_Basic/Assets/Scripts/Bullet.cs
@@ -6,41 +6,46 @@ public class Bullet : MonoBehaviour
 {
     // 1. 앞으로 나가기
     // 2. 충돌 감지 + 대상 해치우기
-    public float speed = 5.0f;
-    Rigidbody rigid = null;
+
+    public float speed = 5.0f;      // 총알의 이동 속도
+    Rigidbody rigid = null;         // 움직이는 물체라 rigidbody 추가 + 이동 처리용
 
     // 오브젝트가 생성완료되었을 때 실행
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody>();
+        rigid = GetComponent<Rigidbody>();  //rigidbody 저장
     }
 
     // 최초의 Update가 실행되기 직전에 실행
     private void Start()
     {
-        //rigid.velocity = new Vector3(0, 0, speed);
-        rigid.velocity = Vector3.forward * speed;
+        rigid.velocity = transform.forward * speed; // 물체의 이동 방향과 속도 설정
     }
 
+    // 다른 collider랑 충돌했을 때 실행되는 함수
     private void OnCollisionEnter(Collision collision)
     {
         // 태그 : 특정한 무언가를 확인할 때
         // 인터페이스 : 특정한 카테고리에 속하는 것들을 처리할 때 
 
-        Debug.Log(collision.gameObject.name);
+        //Debug.Log(collision.gameObject.name);
 
+        // 죽일 수 있는 대상이면 죽인다.
         IDead target = collision.gameObject.GetComponent<IDead>();
         if(target != null)
         {
-            target.OnDead();
+            target.OnDead();    // 죽일 수 있는 오브젝트라 죽인다.
         }
 
-        if (rigid.useGravity == false)
+        // 총알이 뭔가와 부딪쳤을 때 일어나야 할 일들
+        if (rigid.useGravity == false)  // 우리가 초기에 세팅한 그대로라면
         {
-            rigid.useGravity = true;
-            rigid.AddForce(collision.GetContact(0).normal * 2.0f, ForceMode.Impulse);            
-            rigid.AddTorque(Vector3.one * 5.0f, ForceMode.Impulse);
-            Destroy(this.gameObject, 3.0f);
+            rigid.useGravity = true;    // 중력을 켜고
+            Vector3 reflect = Vector3.Reflect(transform.forward, collision.GetContact(0).normal); // 반사된 벡터 계산
+            rigid.AddForce(reflect * 2.0f, ForceMode.Impulse); // 부딪쳐서 튕기는 느낌 추가            
+            Vector3 randomDir = new Vector3(Random.value, Random.value, Random.value);  //랜덤 방향 지정            
+            rigid.AddTorque(randomDir * 5.0f, ForceMode.Impulse); // 바닥에 떨어져서 구르는 느낌 추가
+            Destroy(this.gameObject, 3.0f); // 3초 뒤에 사라지기
         }
         
     }
---
+++ 3D_Basic/Assets/Scripts/TraceTurret.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceTurret : MonoBehaviour
+{
+    public GameObject bullet = null;        // 발사할 총알 오브젝트
+    public Transform shotTransform = null;  // 총알이 발사될 위치
+
+    public float interval = 1.0f;           // 총알을 발사하는 간격(방아쇠를 당기는 간격)
+    public int shots = 5;                   // 한번 발사할때 몇연사를 할 것인가
+    public float rateOfFire = 0.1f;         // 연사를 할 때 발사되는 간격
+
+    public float fireAngle = 5.0f;
+    public float smoothness = 3.0f;
+     
+    IEnumerator shotSave;                   // 코루틴용 IEnumerator 저장
+    bool startCouroutine = false;
+
+    Transform target = null;
+
+    //private void Awake()
+    //{
+    //    SphereCollider collider = this.gameObject.AddComponent<SphereCollider>();
+    //}
+
+    // 첫번째 업데이트가 실행되기 직전
+    private void Start()
+    {
+        shotSave = Shot();          // IEnumerator 저장
+        //StartCoroutine(shotSave);   // 저장한 IEnumerator로 코루틴 실행
+    }
+
+    private void Update()
+    {
+        if (target != null)  // 무언가가 트리거 안에 들어와 있다.
+        {
+            LookTarget();   // 방향 돌리기
+            //Debug.Log($"In Angle : {CanShoot()}");
+            if(CanShoot())  // 쏘는 각도 안에 들어왔는지 확인
+            {
+                // 쏘는 각도 안이다.
+                if (!startCouroutine)   // 지금 안 쏘고 있는 중
+                {
+                    StartCoroutine(shotSave);   // 코루틴 시작
+                    startCouroutine = true;     // 코루틴 시작했다는 표시 남김
+                }
+            }
+            else
+            {
+                // 쏘는 각도 밖이다.
+                if(startCouroutine) // 지금 쏘고 있는 중
+                {
+                    StopShoot();    // 발사 중지
+                }
+            }
+        }
+    }
+
+    private bool CanShoot()
+    {
+        // 총구 방향 백터와 터렛에서 플레이어로 가는 방향 백터사이의 각도를 구함
+        float angle = Vector3.Angle(shotTransform.forward, target.position - transform.position);
+        //Debug.Log(angle);
+        return Mathf.Abs(angle) < fireAngle;
+    }
+
+    private void LookTarget()
+    {
+        // 1. LookAt
+        // 해당 트랜스폼이 목표를 바라보는 방향으로 회전시킴
+        // transform.LookAt(target);
+
+        // 2. LookRotation
+        // 특정 방향을 향하는 회전을 생성
+        // Vector3 dir = target.position - transform.position;
+        // transform.rotation = Quaternion.LookRotation(dir);
+
+        // 3. Quaternion.Lerp, Quaternion.Slerp
+        // 보간 : 중간값을 계산
+        // 보간을 응용해서 부드럽게 움직이는 연출이 가능
+        Vector3 dir = target.position - transform.position; //방향백터 계산(터렛->플레이어)
+        dir.y = 0;
+        transform.rotation =
+            Quaternion.Slerp(
+                transform.rotation,             // 시작할때의 회전 상태
+                Quaternion.LookRotation(dir),   // 끝났을 때의 회전 상태
+                smoothness * Time.deltaTime);   // 시작과 끝 사이 지점(0이면 시작, 1이면 끝)
+    }
+
+    IEnumerator Shot()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval - shots * rateOfFire); // 1초-0.1초*5 대기
+            // 총알 연사 시작
+            for (int i = 0; i < shots; i++) 
+            {
+                //GameObject bulletInstance = Instantiate(bullet, shotTransform);  // 총알 생성
+                //bulletInstance.transform.parent = null;
+                Instantiate(bullet, shotTransform.position, shotTransform.rotation);
+
+                yield return new WaitForSeconds(rateOfFire);    // 0.1초 대기
+            }
+        }
+    }
+
+    // 일정 범위 안에 들어왔는지를 확인하는 방법
+    //  1. 플레이어와의 거리를 계산한다.
+    //     단점 : 매 프레임 계산해야 한다.
+    //  2. 트리거를 사용한다.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            target = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            target = null;            
+            StopShoot();    // 범위 밖이면 쏘는 것도 중지
+        }
+    }
+
+    private void StopShoot()
+    {
+        //StopAllCoroutines();
+        StopCoroutine(shotSave);    // 총알 쏘는 코루틴 정지
+        startCouroutine = false;    // 코루틴 정지 중이라고 표시
+    }
+}
