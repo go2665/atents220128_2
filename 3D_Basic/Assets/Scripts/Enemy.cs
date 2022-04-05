07808b51b0141d222f811aa23f8e8587803c56cb
go2665 <77224762+go2665@users.noreply.github.com>
2022-04-05 14:21:13 +0900

    적 만들기

--- 3D_Basic/Assets/Scripts/Enemy.cs
+++ 3D_Basic/Assets/Scripts/Enemy.cs
@@ -6,8 +6,8 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     NavMeshAgent agent = null;
-    public Transform startPoint = null;
-    public Transform endPoint = null;
+    public Transform[] waypoints = null;
+    int index = 0;
 
     private void Awake()
     {
@@ -17,6 +17,39 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
-        agent.SetDestination(startPoint.position);
+        agent.SetDestination(waypoints[index].position);
     }
+
+    private void Update()
+    {
+        if(CheckArrive())
+        {
+            GoNextWaypoint();
+        }
+    }
+
+    bool CheckArrive()
+    {
+        return agent.remainingDistance <= agent.stoppingDistance;
+    }
+
+    void GoNextWaypoint()
+    {
+        index++;
+        index = index % waypoints.Length;
+        agent.SetDestination(waypoints[index].position);
+    }
+
+    //void Test()
+    //{
+    //    agent.Warp(new Vector3(1,2,3)); // 순간이동 시킬때
+    //    agent.speed;
+    //    agent.acceleration;
+    //    agent.radius;
+    //    agent.avoidancePriority;
+    //    agent.remainingDistance;
+    //    agent.stoppingDistance;
+    //    agent.autoRepath;
+    //    agent.pathPending;              // 경로 계산중인지 아닌지 확인할 때
+    //}
 }
