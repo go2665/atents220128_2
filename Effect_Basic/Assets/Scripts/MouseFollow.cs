54c13ac0c47ede5ead09a7a10d3d3e556307a919
go2665 <77224762+go2665@users.noreply.github.com>
2022-04-12 13:20:21 +0900

    라인, 트레일 씬 만들기

--- Effect_Basic/Assets/Scripts/MouseFollow.cs
+++ Effect_Basic/Assets/Scripts/MouseFollow.cs
@@ -15,11 +15,17 @@ public class MouseFollow : MonoBehaviour
     private void Update()
     {
         //Input.mousePosition;  //InputManager에서 마우스 스크린 좌표 받아오는 방법
-        //Debug.Log(Mouse.current.position.ReadValue());
+
+        //InputSystem을 사용해 마우스의 스크린좌표를 받아오기
         Vector3 mousePosition = Mouse.current.position.ReadValue();
-        mousePosition.z = distance;
+
+        // 거리를 떨어트리지 않으면 카메라 위치로 이동해 카메라에 찍히지 않기 때문에 거리를 벌린다.(최소 near 이상)
+        mousePosition.z = distance;   
+        
+        // 스크린 좌표를 월드 좌표로 변환
         Vector3 target = Camera.main.ScreenToWorldPoint(mousePosition);
 
+        // 해당 목적지로 이동
         transform.position = Vector3.Lerp(transform.position, target, speed);
     }
 }
