1999755a6da39e50d41618f7563364eb21acf25c
go2665 <77224762+go2665@users.noreply.github.com>
2022-04-19 10:25:13 +0900

    4/18 과제 결과

---
+++ 3D_Character/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject target = null;
+    public float moveSpeed = 3.0f;
+
+    private Animator anim = null;
+    private CharacterController controller = null;
+
+    private Vector3 inputDir = Vector3.zero;
+
+    private void Start()
+    {
+        SetTarget(target);
+    }
+
+    public void SetTarget(GameObject _newTarget)
+    {
+        if (_newTarget != null)
+        {
+            target = _newTarget;
+            anim = target.GetComponent<Animator>();
+            controller = target.GetComponent<CharacterController>();
+        }
+        else
+        {
+            target = null;
+            anim = null;
+            controller = null;
+        }
+    }
+
+    private void OnValidate()
+    {
+        SetTarget(target);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputDir = context.ReadValue<Vector2>();        
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("Speed",inputDir.y);
+        controller.Move(target.transform.forward * inputDir.y * Time.deltaTime * moveSpeed);
+    }
+
+}
