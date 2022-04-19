ac9fb11869a2314c4366660cff28698013d6ffa9
go2665 <77224762+go2665@users.noreply.github.com>
2022-04-19 14:13:43 +0900

    입력방향으로 이동
    카메라가 Y축 방향으로 회전하지 않은 경우

--- 3D_Character/Assets/Scripts/Player.cs
+++ 3D_Character/Assets/Scripts/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour, IControllable
 {
     public float moveSpeed = 3.0f;
+    public float turnSpeed = 0.3f;
 
     private Animator anim = null;
     private CharacterController controller = null;
-    private Vector2 inputDir = Vector2.zero;
+    private Vector3 inputDir = Vector2.zero;
+    private Quaternion targetRotation = Quaternion.identity;
 
     public void ControllerConnect()
     {
@@ -18,14 +20,31 @@ public class Player : MonoBehaviour, IControllable
 
     public void MoveInput(Vector2 dir)
     {
-        inputDir = dir;
+        // dir.x : a(-1) d(+1)
+        // dir.y : s(-1) w(+1)
+
+        //Vector3 a = Camera.main.transform.right;
+        //Vector3 b = Vector3.up;
+
+        inputDir.x = dir.x;
+        inputDir.y = 0;
+        inputDir.z = dir.y;
+        inputDir.Normalize();
+
+        if (inputDir.sqrMagnitude > 0.0f)
+        {
+            targetRotation = Quaternion.LookRotation(inputDir); 
+        }
+
     }
 
     public void MoveUpdate()
     {
-        anim.SetFloat("Speed",inputDir.y);
-        //CollisionFlags flags = controller.Move(transform.forward * inputDir.y * Time.deltaTime * moveSpeed);
-        controller.SimpleMove(transform.forward * inputDir.y * moveSpeed);
-        //Debug.Log(isGround);
+        //Debug.Log(inputDir.sqrMagnitude);
+        anim.SetFloat("Speed",inputDir.sqrMagnitude);
+        controller.SimpleMove(inputDir * moveSpeed);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
     }
+
 }
