164cc5eaeeb07a8468594782675c13883b33cc16
go2665 <77224762+go2665@users.noreply.github.com>
2022-04-20 13:45:18 +0900

    게임 메니저 추가

--- 3D_Character/Assets/Scripts/StateMachine/ArmsEquipState.cs
+++ 3D_Character/Assets/Scripts/StateMachine/ArmsEquipState.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class ArmsEquipState : StateMachineBehaviour
 {
-    Player player = null;
-
-    private void Awake()
-    {
-        player = FindObjectOfType<Player>();
-    }
-
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         //Debug.Log($"Enter : {stateInfo.shortNameHash}");
-        player.ArmsEquip(true);
+        GameManager.Inst.MainPlayer.ArmsEquip(true);
     }
 }
--- 3D_Character/Assets/Scripts/StateMachine/ArmsNonEquipState.cs
+++ 3D_Character/Assets/Scripts/StateMachine/ArmsNonEquipState.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 
 public class ArmsNonEquipState : StateMachineBehaviour
 {
-    Player player = null;
-
-    private void Awake()
-    {
-        player = FindObjectOfType<Player>();
-    }
-
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         //Debug.Log($"Enter : {stateInfo.shortNameHash}");
-        player.ArmsEquip(false);
+        GameManager.Inst.MainPlayer.ArmsEquip(false);
     }
 
 }
