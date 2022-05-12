1c986a6ce41400b046b79b8d1308500189cf07c0
go2665 <77224762+go2665@users.noreply.github.com>
2022-05-12 10:47:57 +0900

    Update Inventory.cs
    주석 추가

--- 3D_Character/Assets/Scripts/Inventory/Inventory.cs
+++ 3D_Character/Assets/Scripts/Inventory/Inventory.cs
@@ -22,27 +22,36 @@ public class Inventory
     ItemSlot[] slots = null;                // 아이템 칸 역할을 할 클래스
     const int DEFAULT_INVENTORY_SIZE = 5;   // 기본 아이템 칸 수
 
-    Queue<ItemSlot> emptySlotQueue = null;
+    Queue<ItemSlot> emptySlotQueue = null;  // 비어있는 슬롯을 관리하는 큐
 
+    /// <summary>
+    /// 인벤토리에 들어갈 슬롯과 빈슬롯 큐 초기화
+    /// </summary>
+    /// <param name="size">인벤토리의 칸수. 기본값 5. </param>
     public Inventory(int size = DEFAULT_INVENTORY_SIZE) //생성자
     {        
-        slots = new ItemSlot[size];     // size에 지정된 숫자만큼 슬롯을 만든다.
-        emptySlotQueue = new Queue<ItemSlot>(size);
+        slots = new ItemSlot[size];                     // size에 지정된 숫자만큼 슬롯을 만든다.
+        emptySlotQueue = new Queue<ItemSlot>(size);     // 빈 슬롯을 관리하는 큐의 기본 크기 설정
         for (int i=0; i<size; i++)
         {
-            slots[i] = new ItemSlot();
-            emptySlotQueue.Enqueue(slots[i]);
+            slots[i] = new ItemSlot();                  // 슬롯 생성
+            emptySlotQueue.Enqueue(slots[i]);           // 생성한 슬롯은 우선 빈슬롯큐에 추가
         }
     }
 
+    /// <summary>
+    /// 인벤토리에 아이템을 추가
+    /// </summary>
+    /// <param name="itemData">추가될 아이템의 종류</param>
+    /// <returns>아이템 추가가 성공하면 true. 아니면 false </returns>
     public bool AddItem(ItemData itemData)
     {
         bool result = false;
-        ItemSlot empty = FindEmptySlot();
+        ItemSlot empty = FindEmptySlot();       // 빈슬롯큐에서 비어있는 슬롯 가져오기 시도
         if( empty != null )
         {
             // 비어있는 아이템 슬롯을 가져왔다.
-            empty.slotItem = itemData;      // 수정 필수
+            empty.slotItem = itemData; // 수정 필수
             result = true;
             Debug.Log($"{itemData.name}을 인벤토리에 추가.");
         }
@@ -55,13 +64,17 @@ public class Inventory
         return result;
     }
 
+    /// <summary>
+    /// 비어있는 아이템 칸을 돌려주는 함수
+    /// </summary>
+    /// <returns>null이면 비어있는 슬롯이 없음. 아닌 경우 비어있는 슬롯. </returns>
     ItemSlot FindEmptySlot()
     {
         ItemSlot slot = null;
-        if (emptySlotQueue.Count > 0)   //큐에 남은 슬롯이 있을 때
+        if (emptySlotQueue.Count > 0)           //큐에 남은 슬롯이 있을 때
         {
             slot = emptySlotQueue.Dequeue();    //빈 슬롯을 하나 할당
         }
-        return slot;
+        return slot;    // null 또는 빈 슬롯을 리턴
     }
 }
