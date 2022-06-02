07e7682499e032f88d9410f4d6e3f55d6d0ef88f
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-02 10:39:48 +0900

    TopDown - 포탄 작성(Shell_Cluster)

--- TopDown3D/Assets/Scripts/Shell_Cluster.cs
+++ TopDown3D/Assets/Scripts/Shell_Cluster.cs
@@ -4,4 +4,51 @@ using UnityEngine;
 
 public class Shell_Cluster : Shell
 {
+    public float lifeTime = 1.0f;
+    public float upPower = 20.0f;
+
+    Shell_Submunition[] submunitions = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        submunitions = GetComponentsInChildren<Shell_Submunition>();
+        foreach(Shell_Submunition sub in submunitions)
+        {
+            sub.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        //Time.timeScale = 0.2f;
+
+        StartCoroutine(TimeOut());
+    }
+
+    // 포탄이 위로 날아간다.
+    private void FixedUpdate()
+    {
+        rigid.AddForce(Vector3.up * upPower);     // 계속 위쪽으로 힘을 가하고
+        rigid.MoveRotation(Quaternion.LookRotation(rigid.velocity));    // 이동 방향을 바라보도록 수정
+    }
+
+    protected override void Explotion(Vector3 up)
+    {
+        foreach(Shell_Submunition sub in submunitions)
+        {
+            sub.transform.parent = null;
+            sub.gameObject.SetActive(true);            
+            sub.RandomSpread(-up);
+        }
+        base.Explotion(up);
+    }
+
+    IEnumerator TimeOut()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        //Time.timeScale = 0.0f;
+        Explotion(Vector3.up);
+    }
 }
---
+++ TopDown3D/Assets/Scripts/Shell_Submunition.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell_Submunition : Shell
+{
+    public void RandomSpread(Vector3 downDir)
+    {
+        Vector2 random = Random.insideUnitCircle;
+        Vector3 temp = (downDir + new Vector3(random.x, 0, random.y)).normalized * initialSpeed;
+        rigid.velocity = temp;
+        Debug.Log($"{gameObject.name} : {rigid.velocity}");
+    }
+}
