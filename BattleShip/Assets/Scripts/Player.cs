8c5ac1e9131d312e9e3bef9ffd87165584a5a16a
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-16 10:34:53 +0900

    Battleship - Player 클래스 작성 중(강제 공격 완료)

--- BattleShip/Assets/Scripts/Player.cs
+++ BattleShip/Assets/Scripts/Player.cs
@@ -8,9 +8,12 @@ public class Player : MonoBehaviour
 
     //Ship[] ships = null;                // 플레이어의 함선
     BattleField myField = null;         // 나의 필드
-    BattleField enemyField = null;      // 적의 필드
+    public BattleField enemyField = null;      // 적의 필드
     
     bool isTurnActionFinish = false;    // 턴 동작이 끝났는지 여부
+
+    List<int> randomList = null;        // 다음 랜덤 공격 위치
+
     public bool IsTurnActionFinish
     {
         get => isTurnActionFinish;
@@ -25,25 +28,52 @@ public class Player : MonoBehaviour
     {
         myField = my;
         enemyField = enemy;
+
+        List<int> original = new List<int>(100);
+        for (int i = 0; i < 100; i++)
+        {
+            original.Add(i);
+        }
+
+        randomList = new List<int>(100);
+        while (original.Count > 0)
+        {
+            int index = Random.Range(0, original.Count);
+            int value = original[index];
+            randomList.Add(value);
+            original.RemoveAt(index);
+        }
     }
 
     public void Attack(Vector2Int pos)
     {
+        //Debug.Log("일반 공격");
+        int posValue = pos.y * BattleField.FieldSize + pos.x;
+        randomList.Remove(posValue);
+
         enemyField.Attacked(pos);
     }
 
     public void ForcedAttack()
     {
-        bool find = false;
-        Vector2Int pos = new Vector2Int();
-        while (!find)
-        {
-            pos.x = Random.Range(0, BattleField.FieldSize);
-            pos.y = Random.Range(0, BattleField.FieldSize);
+        //bool find = false;
+        //Vector2Int pos = new Vector2Int();
+        //while (!find)
+        //{
+        //    pos.x = Random.Range(0, BattleField.FieldSize);
+        //    pos.y = Random.Range(0, BattleField.FieldSize);
 
-            find = enemyField.IsAttackable(pos);
-        }
+        //    find = enemyField.IsAttackable(pos);
+        //}
+        //enemyField.Attacked(pos);
+
+        //Debug.Log("강제 공격");
+        int posValue = randomList[0];
+        randomList.RemoveAt(0);
+
+        Vector2Int pos = new Vector2Int(posValue%BattleField.FieldSize, posValue/BattleField.FieldSize);
         enemyField.Attacked(pos);
+
     }
 
     private void Start()
--- BattleShip/Assets/Scripts/Test_Player.cs
+++ BattleShip/Assets/Scripts/Test_Player.cs
@@ -10,6 +10,19 @@ public class Test_Player : MonoBehaviour
 
     void Start()
     {
+        //Test_Attack();
+        StartCoroutine(StartDelay());
+        
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Test_Attack();
+    }
+
+    private void Test_Attack()
+    {
         enemyField.ShipDeployment(new Vector2Int(0, 0), ships[0]);
         enemyField.ShipDeployment(new Vector2Int(3, 3), ships[1]);
         enemyField.ShipDeployment(new Vector2Int(5, 5), ships[2]);
@@ -17,7 +30,7 @@ public class Test_Player : MonoBehaviour
         ships[4].Rotate();
         enemyField.ShipDeployment(new Vector2Int(9, 9), ships[4]);
 
-        //player.enemyField = enemyField;
+        player.enemyField = enemyField;
         //player.Attack(new Vector2Int(0, 0));    // 공격 받음
         //player.Attack(new Vector2Int(1, 0));    // 바다
         //player.Attack(new Vector2Int(0, 1));    // 공격 받음
@@ -25,6 +38,12 @@ public class Test_Player : MonoBehaviour
         //player.Attack(new Vector2Int(0, 3));    // 공격 받음
         //player.Attack(new Vector2Int(0, 4));    // 공격 받음
         //player.Attack(new Vector2Int(0, 5));    // 바다
+
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    player.ForcedAttack();
+        //}
     }
+
 
 }
